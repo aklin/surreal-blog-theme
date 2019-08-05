bee2dd2b27e181cc88ae5e5071dfeff34a8deb6a
Tony Klinakis <a.klinakis@yahoo.com>
2019-08-05 23:01:20 +0100

    Add scss support

---
+++ src/container/theme.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+
+function Theme () {
+
+    return (
+        <Container fluid >
+            <Row>
+                <Col>1 of 2</Col>
+                <Col>2 of 2</Col>
+            </Row>
+        </Container>
+    )
+};
+
+
+export default Theme;
