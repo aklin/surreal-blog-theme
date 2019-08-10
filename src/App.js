aea3d2edc80a7f673b877d539b518d621c789c42
Tony Klinakis <a.klinakis@yahoo.com>
2019-08-10 11:27:35 +0100

    Add eslint, prettify support

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 import Theme from "./container/theme";
+import { SurrealNavbar } from "./component/SurrealNavbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Welcome
-      </header>
-      <nav>nav bar</nav>
-      <Theme/>
+      <header className="App-header">Welcome</header>
+      <SurrealNavbar />
+      <Theme />
     </div>
   );
 }
--- src/container/theme.js
+++ src/container/theme.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-
-function Theme () {
-
-    return (
-        <Container fluid >
-            <Row>
-                <Col>1 of 2</Col>
-                <Col>2 of 2</Col>
-            </Row>
-        </Container>
-    )
-};
-
+function Theme() {
+  return (
+    <Container fluid className="root">
+      <Row className="rootRow">
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+    </Container>
+  );
+}
 
 export default Theme;
