539387c40cada5c3d48cc1cbdcbc8425294b0754
Tony Klinakis <a.klinakis@yahoo.com>
2019-08-10 12:17:35 +0100

    final commit before going to bed

--- src/container/theme.js
+++ src/container/theme.js
@@ -2,22 +2,20 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {SurrealNavbar} from "../component/SurrealNavbar";
+import { SurrealNavbar } from "../component/SurrealNavbar";
 
 function Theme() {
   return (
     <>
-
-        <Container fluid className="root">
-            <Container  className="theme">
-            <SurrealNavbar />
-            <Row className="rootRow">
-
-                <Col className="sidebar" >1 of 2</Col>
-                <Col>2 of 2</Col>
-            </Row>
-            </Container>
+      <Container fluid className="root">
+        <Container className="theme">
+          <SurrealNavbar />
+          <Row className="rootRow">
+            <Col className="sidebar">1 of 2</Col>
+            <Col>2 of 2</Col>
+          </Row>
         </Container>
+      </Container>
     </>
   );
 }
