56f7b72aaabb628ebfed74a9b491a4ef0b78e07a
Tony Klinakis <a.klinakis@yahoo.com>
2019-08-10 12:09:41 +0100

    some refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Theme from "./container/theme";
-import { SurrealNavbar } from "./component/SurrealNavbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Welcome</header>
-      <SurrealNavbar />
       <Theme />
     </div>
   );
--- src/component/SurrealNavbar.js
+++ src/component/SurrealNavbar.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Navbar, Nav, FormControl, Button, Form } from "react-bootstrap";
+import {Navbar, Nav, FormControl, Button, Form, Container} from "react-bootstrap";
 
 export const SurrealNavbar = () => (
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-    </Form>
-  </Navbar>
+    <Navbar bg="dark" variant="dark" >
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+        </Form>
+    </Navbar>
 );
