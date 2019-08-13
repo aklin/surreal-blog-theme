3a6cc8c3aa76d2c31f3f1ebda6ebe7a9f8dcd74a
Tony Klinakis <a.klinakis@yahoo.com>
2019-08-13 21:06:59 +0100

    more theming

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -4,20 +4,24 @@ import { Form, FormControl } from "react-bootstrap";
 
 export const Sidebar = () => (
   <Col className="sidebar mh-100" md={3} sm={1}>
-    <div className="search">
-      <Form inline>
-        <FormControl
-          type="text"
-          placeholder="Search"
-          className="mr-sm-2 w-100 navSearch"
-        />
-      </Form>
+    <div className="sidebar-item">
+      <div className="search">
+        <Form inline>
+          <FormControl
+            type="text"
+            placeholder="Search"
+            className="mr-sm-2 w-100 navSearch"
+          />
+        </Form>
+      </div>
     </div>
-    <div className="title">
-      <h3>Hello, my name is</h3>
-      <h1>Tony Klinakis</h1>
-      <h3>and this is my</h3>
-      <h2>blog.</h2>
+    <div className="sidebar-item">
+      <div className="title">
+        <h3>Hello, my name is</h3>
+        <h1>Tony Klinakis</h1>
+        <h3>and this is my</h3>
+        <h2>blog.</h2>
+      </div>
     </div>
   </Col>
 );
