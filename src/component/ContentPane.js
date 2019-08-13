0019d0fd11d0572c6edeed9560d31cddd2ea46a9
Tony Klinakis <a.klinakis@yahoo.com>
2019-08-13 01:24:19 +0100

    Lots of progress. Get rid of navbar.

---
+++ src/component/ContentPane.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+
+export const ContentPane = () => (
+    <Col className="content">
+        hello
+    </Col>
+);
--- src/container/Theme.js
+++ src/container/Theme.js
@@ -1,23 +1,17 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { SurrealNavbar } from "../component/SurrealNavbar";
+import {Sidebar} from "../component/Sidebar";
+import {ContentPane} from "../component/ContentPane";
 
 function Theme() {
   return (
     <>
       <Container fluid className="root">
         <Container fluid className="theme">
-          <SurrealNavbar />
           <Row className="rootRow">
-            {/*col={3}*/}
-            <Col className="sidebar mh-100" sm={3}>
-              .col-6 .col-sm-3
-            </Col>
-            <Col className="content" col={6} sm>
-              2 of 2
-            </Col>
+            <Sidebar/>
+            <ContentPane/>
           </Row>
         </Container>
       </Container>
